f02ed7e17645f92d9855b20f14e38410fe1b2d7a
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-08-25 17:13:47 -0400

    Intro to Flask With JWT

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from .models import db, User
+from models import db, User
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
 
